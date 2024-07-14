ecfd00a3a6c8b37d2ab98d602cc5caac914fe634
reallylanks <brendonsmith8991@gmail.com>
2024-07-13 20:12:57 -0400

    feat: Add display and buttons to calculator interface - Implemented a display area for the calculator. Added buttons for calculator operations and numbers. Encountering an issue where some buttons are not being created.

--- calculator.js
+++ calculator.js
@@ -14,7 +14,7 @@ const divide = function(num1, num2) {
     return (num1 / num2)
 };
 
-console.log(add(100, 2));
+console.log(divide(100, 2));
 
 let num1;
 let num2;
@@ -26,8 +26,46 @@ const operate = function(num1, num2, operator) {
     } else if (operator === subtract) {
         return subtract(num1, num2);
     } else if (operator === multiply) {
-        return multiply(num1, num2)
+        return multiply(num1, num2);
     } else {
         return divide(num1, num2);
     }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonContainer = document.getElementById('buttonContainer');
+    const buttons = [
+        {text: 'AC', class: 'special'},
+        {text: '+/-', class: 'special'},
+        {text: '%', class: 'special'},
+        {text: '/', class: 'operator'},
+        {text: '7', class: 'number'},
+        {text: '8', class: 'number'},
+        {text: '9', class: 'number'},
+        {text: '*', class: 'operator'},
+        {text: '4', class: 'number'},
+        {text: '5', class: 'number'},
+        {text: '6', class: 'number'},
+        {text: '-', class: 'operator'},
+        {text: '1', class: 'number'},
+        {text: '2', class: 'number'},
+        {text: '3', class: 'number'},
+        {text: '+', class: 'operator'},
+        {text: '0', class: 'wide number'},
+        {text: '.', class: 'number'},
+        {text: '=', class: 'operator'}
+    ];
+    
+
+    buttons.forEach(buttonInfo => {
+        const button = document.createElement('button');
+        button.innerText = buttonInfo.text;
+        button.classList.add(buttonInfo.class);
+        button.addEventListener('click', () => {
+            /* this doesn't do anything, need to find 
+            a way to return to the display */
+            return button.innerText;
+        });
+        buttonContainer.appendChild(button);
+    })
+});
