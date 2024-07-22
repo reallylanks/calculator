c492ac1053cf73df6062ee56b9213ecf84547694
reallylanks <brendonsmith8991@gmail.com>
2024-07-21 22:32:49 -0400

    Basic, singular equations working. Need to add better decimal functionality and complex equation functionality.

--- calculator.js
+++ calculator.js
@@ -1,38 +1,48 @@
 const add = function(num1, num2) {
-    return (num1 + num2)
+    return (num1 + num2);
 };
 
 const subtract = function(num1, num2) {
-	return (num1 - num2)
+	return (num1 - num2);
 };
 
 const multiply = function(num1, num2) {
-    return (num1 * num2)
+    return (num1 * num2);
 };
 
 const divide = function(num1, num2) {
-    return (num1 / num2)
+    return (num1 / num2);
 };
 
+const updateDisplay = function() {
+    document.getElementById('display').innerText = `${displayValue1}`;
+}
 
+let displayValue1 = '0';
 let num1;
 let num2;
 let operator; 
 
 const operate = function(num1, num2, operator) {
-    if (operator === add) {
-        return add(num1, num2);
-    } else if (operator === subtract) {
-        return subtract(num1, num2);
-    } else if (operator === multiply) {
-        return multiply(num1, num2);
-    } else {
-        return divide(num1, num2);
+    let result;
+    if (operator === '+') {
+        result = add(num1, num2);
+    } else if (operator === '-') {
+        result = subtract(num1, num2);
+    } else if (operator === '*') {
+        result = multiply(num1, num2);
+    } else if (operator === '/') {
+        result = divide(num1, num2);
     }
+
+    return result.toString();
+
 };
 
 document.addEventListener('DOMContentLoaded', function() {
     const buttonContainer = document.getElementById('buttonContainer');
+    const display = document.getElementById('display');
+
     const buttons = [
         {text: 'AC', class: 'special'},
         {text: '+/-', class: 'special'},
@@ -55,18 +65,80 @@ document.addEventListener('DOMContentLoaded', function() {
         {text: '=', class: 'operator'}
     ];
     
-
-    
     buttons.forEach(buttonInfo => {
-        
         const button = document.createElement('button');
         button.innerText = buttonInfo.text;
         button.classList.add(buttonInfo.class);
         button.addEventListener('click', () => {
-            /* this doesn't do anything, need to find 
-            a way to return to the display */
-            return button.innerText;
+            
+            if (buttonInfo.class === 'number' || buttonInfo.class === 'wide-number') {
+
+                if (displayValue1.charAt(0) === '0') {
+                    displayValue1 = buttonInfo.text;
+                    updateDisplay();
+                }
+
+                else if (num1 == displayValue1 && operator !== undefined) {
+                    displayValue1 = buttonInfo.text;
+                    updateDisplay();
+                }
+
+                else {
+                    displayValue1 += buttonInfo.text;
+                    updateDisplay();
+                } 
+            }
+            
+            if (buttonInfo.class === 'operator') {
+                
+                if (operator === undefined) {
+                    operator = buttonInfo.text;
+                }
+                
+                
+                if (num1 === undefined) {
+                    num1 = Number(displayValue1);
+                }
+
+                if (buttonInfo.text === '=' && num1 !== undefined) {
+                    num2 = Number(displayValue1);
+                    displayValue1 = operate(num1, num2, operator);
+                    num1 = Number(displayValue1);
+                    num2 = undefined;
+                    operator = undefined;
+                    updateDisplay();
+                }
+                
+            }
+
+            if (buttonInfo.class === 'special') {
+                if (buttonInfo.text === 'AC') {
+                    displayValue1 = '0';
+                    num1 = undefined;
+                    num2 = undefined;
+                    updateDisplay();
+                }
+            }
+
+
+            /*so
+            NOTES FOR NEXT SESSIONN
+            CURRENTLY CAN DO 1+1=2, + 2 = 4
+            NEED EVERYTHING TO RESET IF A NUMBER IS PRESSED AFTER '='
+            currently just adds numbers to display lol
+
+            ALSO NEED A WAY TO ADD NUMBERS TO ZERO WHEN DECIMAL IS PRESSED
+            CURRENTLY OUTPUTS JUST '.4' INSTEAD OF '0.4'
+            } */
+            
+            console.log(`num 1 value: ${num1}`);
+            console.log(`num 2 value: ${num2}`);
+            console.log(`Current operator: ${operator}`);
+            
+            
         });
+
+        // add function for buttons to be lighter on hover, also add white on click
         buttonContainer.appendChild(button);
     })
 });
