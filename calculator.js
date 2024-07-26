98a86ae4b51b57d92bfc6dad2b53ad31bc6df55c
reallylanks <brendonsmith8991@gmail.com>
2024-07-26 18:48:19 -0400

    Support for regular and subsequent equations. Need to improve decimal functionality and limit character input.

--- calculator.js
+++ calculator.js
@@ -72,43 +72,86 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', () => {
             
             if (buttonInfo.class === 'number' || buttonInfo.class === 'wide-number') {
-
-                if (displayValue1.charAt(0) === '0') {
+                // if '.' is pressed and character 1 is 0, add '.'
+                if (displayValue1.charAt(0) === '0' && buttonInfo.text === '.') {
+                    displayValue1 += buttonInfo.text;
+                    updateDisplay();
+                }
+               // if first char is 0, the pressed number replaces it 
+                else if (displayValue1.charAt(0) === '0') {
                     displayValue1 = buttonInfo.text;
                     updateDisplay();
                 }
-
+                // if num1 is equal to display and operator is NOT undefined, display now equals button text value    
                 else if (num1 == displayValue1 && operator !== undefined) {
                     displayValue1 = buttonInfo.text;
                     updateDisplay();
-                }
+                } 
 
-                else {
+                // if calc is fresh or if num2 is 'ready4next', add pressed number to display
+                else if ((num1 === undefined && num2 === undefined && operator === undefined) || (num1 !== undefined && num2 === 'ready4Next' )) {
                     displayValue1 += buttonInfo.text;
                     updateDisplay();
                 } 
+                // START OF A NEW EQUATION: if num2 and operator are 'ready4Next', button pressed resets calc and becomes display value
+                if (num2 === 'ready4Next' && operator === 'ready4Next') {
+                    displayValue1 = buttonInfo.text
+                    num1 = undefined;
+                    num2 = undefined;
+                    operator = undefined;
+                    updateDisplay();
+                }
+
+               /*  else {
+                    displayValue1 += buttonInfo.text;
+                    updateDisplay();
+                }  */
             }
             
             if (buttonInfo.class === 'operator') {
-                
-                if (operator === undefined) {
-                    operator = buttonInfo.text;
-                }
-                
                 
                 if (num1 === undefined) {
                     num1 = Number(displayValue1);
                 }
 
-                if (buttonInfo.text === '=' && num1 !== undefined) {
+                if (operator === undefined || operator === 'ready4Next') {
+                    operator = buttonInfo.text;
+                }
+
+                else if (buttonInfo.text !== '=' && operator !== undefined) {
                     num2 = Number(displayValue1);
                     displayValue1 = operate(num1, num2, operator);
                     num1 = Number(displayValue1);
-                    num2 = undefined;
-                    operator = undefined;
-                    updateDisplay();
+                    num2 = 'ready4Next';
+                    operator = buttonInfo.text;
+                    updateDisplay(); 
                 }
                 
+
+                else if (buttonInfo.text === '=' && num1 !== undefined) {
+                    num2 = Number(displayValue1);
+                    displayValue1 = operate(num1, num2, operator);
+                    num1 = Number(displayValue1);
+                    num2 = 'ready4Next';
+                    operator = 'ready4Next';
+                    updateDisplay(); 
+                }
+                
+                
+                
+
+                // this block
+                /* if (num1 !== undefined && operator !== undefined && operator !== 'ready4Next') {
+                    num2 = Number(displayValue1);
+                    operator = buttonInfo.text;
+                    displayValue1 = operate(num1, num2, operator);
+                    num1 = Number(displayValue1);
+                    num2 = 'ready4Next';
+                    operator = 'ready4Next';
+                    updateDisplay();
+                }  */
+                
+        
             }
 
             if (buttonInfo.class === 'special') {
@@ -116,19 +159,60 @@ document.addEventListener('DOMContentLoaded', function() {
                     displayValue1 = '0';
                     num1 = undefined;
                     num2 = undefined;
+                    operator = undefined;
                     updateDisplay();
+                }
+
+                if (buttonInfo.text === '%') {
+                    result = Number(displayValue1) * 0.01;
+                    displayValue1 = result.toString();
+                    updateDisplay();
+                }
+
+                if (buttonInfo.text === '+/-') {
+                    if (displayValue1.charAt(0) === '-') {
+                        displayValue1 = displayValue1.replace('-', '');
+                        updateDisplay();
+                    }
+
+                    else {
+                        displayValue1 = '-' + displayValue1;
+                        updateDisplay();
+                    }
                 }
             }
 
 
             /*so
-            NOTES FOR NEXT SESSIONN
+            NOTES FOR NEXT SESSIONN 1 
             CURRENTLY CAN DO 1+1=2, + 2 = 4
             NEED EVERYTHING TO RESET IF A NUMBER IS PRESSED AFTER '='
             currently just adds numbers to display lol
 
+                Maybe need to let operator and num2 equal to something other 
+                than undefined so it's easy to detect when doing a second equation
+                 !! ready4Next !!
+
             ALSO NEED A WAY TO ADD NUMBERS TO ZERO WHEN DECIMAL IS PRESSED
             CURRENTLY OUTPUTS JUST '.4' INSTEAD OF '0.4'
+
+
+            ---------------------------------------------------------------------
+            NOTES FOR SESSION 2 
+            Can finally do subsequent equations (hooray!)
+
+            I broke something else lol
+            num1 + num 2 = 
+            then + num 2 
+            breaks the calculator (happens when i click num2, 
+            so must be a number button rule)...sets num2 same as num1???
+            if '=' is clicked, operator needs to be reset every time
+
+            NEED TO LIMIT INPUT TO ONLY ONW '.'
+            NEED TO LIMIT AMOUNT OF CHARACTERS TO 10
+                Round bigger answers?
+                disable buttons!
+
             } */
             
             console.log(`num 1 value: ${num1}`);
